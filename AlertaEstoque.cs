5b8eecda68c2969278d94bae02c60aa45727368a
CesarGeek <cesar.work10@gmail.com>
2018-04-23 22:47:02 -0300

    Atividade sobre o Alerta de Estique

---
+++ AlertaEstoque.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Alerta_Estoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double qtdMaxima,qtdAtual,resultados;
+            string nomeProduto;
+
+            Console.Clear();
+
+            Console.WriteLine(">>>>>>>>>>>>>>> Olá, Bem Vindo ao Controle de Estoque <<<<<<<<<<<<<<<");
+            
+
+            Console.WriteLine("Digite o Nome de Produto");
+            nomeProduto = Console.ReadLine();
+            
+            Console.WriteLine("Digite a Quantidade Atual do Produto");
+            qtdAtual = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a Quantidade Máxima de Produto");
+            qtdMaxima = double.Parse(Console.ReadLine());
+
+            resultados = (qtdAtual / qtdMaxima)*100;
+
+            Console.WriteLine(resultados.ToString("n2")+"%");
+
+            if(resultados <= 12)
+                Console.WriteLine("Comprar com Urgência");
+            else if(resultados<=20)
+                Console.WriteLine("Realizar Compra");
+            else
+                Console.WriteLine("Ponto de Utilização");
+
+
+
+        }
+    }
+}
